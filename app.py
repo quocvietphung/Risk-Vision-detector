4d2cebbd8f2f7ec8a9695df9e9bd807c441ce1d4
vietphung <harryportervn1993@gmail.com>
2025-06-25 00:10:31 +0200

    add API endpoints for transaction retrieval and insertion

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, send_from_directory
-import os
+from flask import request, jsonify
+from model.transaction import Transaction
+tx_model = Transaction()
 
 app = Flask(__name__, static_folder="fraud-detection/dist", static_url_path="/")
 
@@ -16,6 +18,23 @@ def serve_static(path):
 @app.route("/api/hello")
 def hello_api():
     return {"message": "Hello from Flask API!"}
+
+@app.route("/api/transactions", methods=["GET"])
+def get_transactions():
+    data = tx_model.fetch_all()
+    return jsonify(data)
+
+@app.route("/api/transactions", methods=["POST"])
+def add_transaction():
+    req = request.json
+    tx_model.insert(
+        time=req["time"],
+        amount=req["amount"],
+        risk=req["risk"],
+        is_fraud=req["is_fraud"],
+        predicted_score=req["predicted_score"]
+    )
+    return jsonify({"message": "Inserted successfully"}), 201
 
 
 if __name__ == "__main__":
