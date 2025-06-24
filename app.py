6b1dd6d834f50918c09c587c80c330f6973f75ba
vietphung <harryportervn1993@gmail.com>
2025-06-25 00:22:47 +0200

    add CSV upload endpoint to process transaction data and calculate fraud statistics

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_from_directory
 from flask import request, jsonify
 from model.transaction import Transaction
+import pandas as pd
 tx_model = Transaction()
 
 app = Flask(__name__, static_folder="fraud-detection/dist", static_url_path="/")
@@ -13,7 +14,6 @@ def serve_frontend():
 @app.route("/<path:path>")
 def serve_static(path):
     return send_from_directory(app.static_folder, path)
-
 
 @app.route("/api/hello")
 def hello_api():
@@ -35,6 +35,23 @@ def add_transaction():
         predicted_score=req["predicted_score"]
     )
     return jsonify({"message": "Inserted successfully"}), 201
+
+@app.route("/api/upload", methods=["POST"])
+def upload_csv():
+    file = request.files['file']
+    df = pd.read_csv(file)
+
+    total = len(df)
+    fraud_count = df[df["Class"] == 1].shape[0]
+    fraud_percent = round((fraud_count / total) * 100, 2)
+    total_amount = round(df["Amount"].sum(), 2)
+
+    return jsonify({
+        "total_transactions": total,
+        "fraud_count": fraud_count,
+        "fraud_percentage": fraud_percent,
+        "total_amount": total_amount
+    })
 
 
 if __name__ == "__main__":
